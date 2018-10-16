33b1344429caa7b24af1f1c2e04b75d6b917f161
Kostya <kostiantynsuprun@gmail.com>
2018-10-16 16:32:09 +0300

    added storage
    added IDataStorage.cs and InMemoryStorage.cs

---
+++ DiscordBotCore/Storage/Implementations/InMemoryStorage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiscordBotCore.Storage.Implementations
+{
+	public class InMemoryStorage : IDataStorage
+	{
+		public InMemoryStorage() {
+			Console.WriteLine("InMemoryStorage constructor");
+		}
+		private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+
+		public void StoreObject (object obj, string key) {
+			if (_dictionary.ContainsKey(key)) {
+				return;
+			}
+			_dictionary.Add(key, obj);
+		}
+		public T RestoreObject<T> (string key) {
+			if (!_dictionary.ContainsKey(key)) {
+				throw new ArgumentException($"the provided '{key}' wasn't found");
+			}
+			return (T)_dictionary[key];
+		}
+		public void Hello() {
+			Console.WriteLine("hi!");
+		}
+	}
+}
